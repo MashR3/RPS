b9bad25372effc7655748f368b37d261d701a087
MashR3 <michaelash17@googlemail.com>
2017-09-11 20:46:03 +0100

    Nearly fin basics

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ if (operation === '2') {
 	var p1 = prompt('Player One! You must decide!' + '\n [r] Rock' + '\n [p] Paper' + '\n [s] Scissors').toLowerCase();
 	var p2 = prompt('Player Two! How will you counterattack?' + "\n [r] Rock" + "\n [p] Paper" + "\n [s] Scissors").toLowerCase();
 
-	if (((p1 === 'r') && (p2 === 'r')) || ((p1 === 'p') && (p2 === 'p')) || ((p1 === 's') && (p2 === 's')));
-		alert('You have tied!! Unacceptable! Go again!');
+	// if (((p1 === 'r') && (p2 === 'r')) || ((p1 === 'p') && (p2 === 'p')) || ((p1 === 's') && (p2 === 's')));
+	// 	alert('You have tied!! Unacceptable! Go again!');
 
-		} else if ((p1 === 'r') && (p2 === 's')){
+	} if ((p1 === 'r') && (p2 === 's')){
 		alert('p1 you are victorious!');
 
 		} else if ((p1 === 'r') && (p2 === 'p')){
@@ -25,12 +25,26 @@ if (operation === '2') {
 		} else if ((p1 === 'p') && (p2 === 'r')){
 		alert('p1 you are victorious!');
 
+		} else if (p1 === p2) {
+			alert('No draws allowed! Again!');
+		
+	}
 
-} else if (operation === "pc") {
+else if (operation === "pc") {
 	var p1 = prompt("Human! I'll give you the first move!" + "\n [r] Rock" + "\n [p] Paper" + "\n [s] Scissors")
 
 
-} else {
-	alert("You must play!!")
-}
+} else 
+	alert("You must play!!");
 
+
+
+
+// 	} else if (adop === "p") {
+// 		var no2 = parseFloat(prompt('Enter base number'));
+// 		var no3 = parseFloat(prompt('Enter number power'));
+// 		alert ("The answer is " + (Math.pow(no2, no3)));
+
+// 	} else {
+//   		alert('Please select a valid operator!');
+// }
