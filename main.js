24d02e5d449824006f2abe08bb29bf9b56cd6f0d
MashR3 <michaelash17@googlemail.com>
2017-09-11 22:55:48 +0100

    This makes no sense

--- main.js
+++ main.js
@@ -1,54 +1,82 @@
 var operation = prompt('Welcome to Rock Paper Scissors! Would you like to play: ' + '\n ' + '\n [2] for two-player?'+ '\n [pc] to play against the computer?').toLowerCase();
 
+
+
+
+function replay() {
+
 if (operation === '2') {
+
 	var p1 = prompt('Player One! You must decide!' + '\n [r] Rock' + '\n [p] Paper' + '\n [s] Scissors').toLowerCase();
-	var p2 = prompt('Player Two! How will you counterattack?' + "\n [r] Rock" + "\n [p] Paper" + "\n [s] Scissors").toLowerCase();
+	var p2 = prompt('Player Two! How will you counterattack?' + '\n [r] Rock' + '\n [p] Paper' + '\n [s] Scissors').toLowerCase();
 
-	// if (((p1 === 'r') && (p2 === 'r')) || ((p1 === 'p') && (p2 === 'p')) || ((p1 === 's') && (p2 === 's')));
-	// 	alert('You have tied!! Unacceptable! Go again!');
+		} if ((p1 === 'r') && (p2 === 's')){
+			alert('Player 1 is victorious!');
 
-	} if ((p1 === 'r') && (p2 === 's')){
-		alert('Player 1 is victorious!');
+			} else if ((p1 === 'r') && (p2 === 'p')){
+			alert('Player 2 is victorious!');
 
-		} else if ((p1 === 'r') && (p2 === 'p')){
-		alert('Player 2 is victorious!');
+			} else if ((p1 === 's') && (p2 === 'r')){
+			alert('PLayer 2 is victorious!');
 
-		} else if ((p1 === 's') && (p2 === 'r')){
-		alert('PLayer 2 is victorious!');
+			} else if ((p1 === 's') && (p2 === 'p')){
+			alert('Player 1 is victorious!');
 
-		} else if ((p1 === 's') && (p2 === 'p')){
-		alert('Player 1 is victorious!');
+			} else if ((p1 === 'p') && (p2 === 's')){
+			alert('Player 2 victorious!')
 
-		} else if ((p1 === 'p') && (p2 === 's')){
-		alert('Player 2 victorious!')
+			} else if ((p1 === 'p') && (p2 === 'r')){
+			alert('Player 1 victorious!');
 
-		} else if ((p1 === 'p') && (p2 === 'r')){
-		alert('Player 1 victorious!');
+			} else if (p1 === p2) {
+				alert('No draws allowed! Again!');
 
-		} else if (p1 === p2) {
-			alert('No draws allowed! Again!');
-		
+			}
+		}
+
+
+
 		var game = true;
 
 		while (game) {
-			var response = prompt('Would you like to play again? [y] or [n]').toLowerCase();
+			var choice = prompt('Would you like to play again? [y] or [n]').toLowerCase();
+
+			if (choice === 'n');
+			 game = false;
 			
-			if (response === "n");
-				game = false;
-		}
+			if (choice === 'y');
+			
+			replay();
 
-	}
+		
+			
+			
+		// while (game) {
+		// 	var response = prompt('Would you like to play again? [y] or [n]').toLowerCase();
+			
+		// 	if (response === "n");
+		// 		game = false;
 
-else if (operation === "pc") {
-	var p1 = prompt("Human! I'll give you the first move!" + "\n [r] Rock" + "\n [p] Paper" + "\n [s] Scissors")
+		// 	if (response === "y");
+		// 		p2game();
+
+		// }
+
+	// }
 
 
-} else 
-	alert("You must play!!");
+
+// else if (operation === "pc") {
+// 	var p3 = prompt("Human! I'll give you the first move!" + "\n [r] Rock" + "\n [p] Paper" + "\n [s] Scissors")
 
 
-// var programRunning = true;
+// } else 
+// 	alert("You must play!!");
 
-// while (programRunning) {
-// 	console.log('The program is running');
-// 	var response = prompt('would you like to end the program? y/n');
+
+
+
+
+
+
+
